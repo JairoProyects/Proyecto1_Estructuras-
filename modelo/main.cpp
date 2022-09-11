af2a5e89a24885eedaa3bf39e24837b64234f689
AndresACV <andres.calvo.vargas@est.una.ac.cr>
2022-09-10 18:54:13 -0600

    Agregacion de hola mundo

---
+++ modelo/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Andre on 9/10/2022.
+//
+
+#ifndef PROYECTO1_ESTRUCTURAS_MAIN_CPP
+#define PROYECTO1_ESTRUCTURAS_MAIN_CPP
+
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hola mundo";
+    return 0;
+}
+
+
+#endif //PROYECTO1_ESTRUCTURAS_MAIN_CPP
