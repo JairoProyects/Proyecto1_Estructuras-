8f5b5ac35bb2c8682a6f560beb56b8cc4c105f90
AndresACV <andres.calvo.vargas@est.una.ac.cr>
2022-09-10 19:19:50 -0600

    HOLA ANDRES

--- modelo/main.cpp
+++ modelo/main.cpp
@@ -5,12 +5,13 @@
 #ifndef PROYECTO1_ESTRUCTURAS_MAIN_CPP
 #define PROYECTO1_ESTRUCTURAS_MAIN_CPP
 
-//
+
 #include <iostream>
 using std::cout, std::endl;
 
 int main(int argc, char* argv[]) {
-    cout << "Hola mundo"<<endl;
+    std::cout << "Hola mundo Andres";
+    std::cout <<"Adios mundo";
     cout<< "Hola mundo_2"<<endl;
     return 0;
 }
