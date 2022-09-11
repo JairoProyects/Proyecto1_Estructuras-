1945dd9a6f7a9690a11229c6f42f01ec021748a0
xojairoxo <jairo.castillo.aragon@est.una.ac.cr>
2022-09-10 19:08:16 -0600

    arregle el codigo

--- modelo/main.cpp
+++ modelo/main.cpp
@@ -5,11 +5,13 @@
 #ifndef PROYECTO1_ESTRUCTURAS_MAIN_CPP
 #define PROYECTO1_ESTRUCTURAS_MAIN_CPP
 
-
+//
 #include <iostream>
+using std::cout, std::endl;
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hola mundo";
+    cout << "Hola mundo"<<endl;
+    cout<< "Hola mundo_2"<<endl;
     return 0;
 }
 
